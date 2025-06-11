ab4651786c00ecfa483f5c17ee2ad5189eb0808d
jana-shams <s-jana.ali@zewailcity.edu.eg>
2025-06-12 02:15:45 +0300

    Add files via upload

---
+++ RestaurantManagementSystem/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestaurantManagementSystem.Models;
+
+namespace RestaurantManagementSystem.Services
+{
+    public class UserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void SetUserSession(string userType, int userId)
+        {
+            _httpContextAccessor.HttpContext.Session.SetString("UserType", userType);
+            _httpContextAccessor.HttpContext.Session.SetInt32("UserId", userId);
+
+        }
+
+        public string? GetUserType()
+        {
+            return _httpContextAccessor.HttpContext.Session.GetString("UserType");
+        }
+        public int? GetUserId()
+        {
+            return _httpContextAccessor.HttpContext.Session.GetInt32("UserId");
+        }
+
+        public bool IsUserAuthenticated()
+        {
+            return GetUserType() != null;
+        }
+        public void ClearUserSession()
+        {
+            _httpContextAccessor.HttpContext.Session.Remove("UserType");
+            _httpContextAccessor.HttpContext.Session.Remove("UserId");
+
+        }
+
+    }
+}
---
+++ RestaurantManagementSystem/ViewModels/CustomerReservationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestaurantManagementSystem.ViewModels
+{
+    public class CustomerReservationViewModel
+    {
+        public int ReservationID { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public int CustomerID { get; set; }
+        public int NumberOfPeople { get; set; }
+    }
+}
